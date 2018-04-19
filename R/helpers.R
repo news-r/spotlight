bdc7b233ef382fff68218fc7bd59362587e88e62
Jean-Philippe Coene <jean-philippe.coene@weforum.org>
2018-04-19 13:55:02 +0200

    filter documents

---
+++ R/helpers.R
@@ -0,0 +1,27 @@
+#' Filter empty documents.
+#'
+#' Filter empty documents.
+#'
+#' @param text vector or list of text.
+#'
+#' @examples
+#' text <- c("Some text", "", "", "More text")
+#'
+#' spot_filter(text)
+#'
+#' @export
+spot_filter <- function(text) UseMethod("spot_filter")
+
+#' @rdname spot_filter
+#' @method spot_filter list
+#' @export
+spot_filter.list <- function(text){
+  lapply(text, function(x) x[nchar(x) > 0L])
+}
+
+#' @rdname spot_filter
+#' @method spot_filter character
+#' @export
+spot_filter.character <- function(text){
+  text[nchar(text) > 0L]
+}
