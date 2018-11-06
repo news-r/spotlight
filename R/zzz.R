3c74c79f75f59e5b7baf8abaa2dce76ce843beb0
JohnCoene <jcoenep@gmail.com>
2018-11-06 15:24:51 +0100

    default sleep changed

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
     SPOTLIGHT_LANGUAGE = "en",
     SPOTLIGHT_RESPONSE = "list",
     SPOTLIGHT_RETRY = 25,
-    SPOTLIGHT_SLEEP = 3,
+    SPOTLIGHT_SLEEP = .5,
     SPOTLIGHT_QUIET = !interactive()
   )
 }
